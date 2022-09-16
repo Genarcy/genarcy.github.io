1aef0e0e55bc0f7f7df211243abd890dae23b29c
0rca <113267753+Genarcy@users.noreply.github.com>
2022-09-15 22:50:45 -0400

    Update sound-onclick.js

--- js/sound-onclick.js
+++ js/sound-onclick.js
@@ -3,7 +3,7 @@ function playclip() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
  {
-  document.all.sound.src = "click.mp3";
+  document.all.sound.src = "Genarcy.mp3";
  }
 }
 
