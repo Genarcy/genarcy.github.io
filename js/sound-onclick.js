65e8e3226d14d70922720eede266ea5c30da83d3
nxxh447 <65509800+nxxh447@users.noreply.github.com>
2022-11-22 08:11:25 -0500

    [EDIT]: .....better code (read desc)
    - what was the previous code lol, why was it like that.

--- js/sound-onclick.js
+++ js/sound-onclick.js
@@ -1,16 +1,6 @@
-
-function playclip() {
-if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
-if (document.all)
- {
-  document.all.sound.src = "/audio/Genarcy.mp3";
- }
+function playClip() {
+	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8") != -1) && document.all) {
+		document.all.sound.src = "/audio/Genarcy.mp3";
+	} else document.getElementsByTagName("audio")[0].play();
 }
 
-else {
-{
-var audio = document.getElementsByTagName("audio")[0];
-audio.play();
-}
-}
-}
