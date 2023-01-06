4c40c7221a914807c3680b06fd2d7da4b4486f42
0rca <113267753+Genarcy@users.noreply.github.com>
2023-01-05 22:30:36 -0500

    d

--- js/app.js
+++ js/app.js
@@ -1,6 +1,138 @@
 
+particlesJS("particles-js", {
+  "particles": {
+      "number": {
+          "value": 80,
+          "density": {
+              "enable": true,
+              "value_area": 800
+          }
+      },
+      "color": {
+          "value": "#d90c0c"
+      },
+      "shape": {
+          "type": "edge",
+          "stroke": {
+              "width": 0,
+              "color": "#000000"
+          },
+          "polygon": {
+              "nb_sides": 5
+          },
+          "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+          }
+      },
+      "opacity": {
+          "value": 0.6707754953874318,
+          "random": true,
+          "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+          }
+      },
+      "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+          }
+      },
+      "line_linked": {
+          "enable": false,
+          "distance": 150,
+          "color": "#000000",
+          "opacity": 0.4,
+          "width": 1
+      },
+      "move": {
+          "enable": true,
+          "speed": 2.8,
+          "direction": "top",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+              "enable": false,
+              "rotateX": 2405.118091298284,
+              "rotateY": 2084.43567912518
+          }
+      }
+  },
+  "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "bubble"
+          },
+          "onclick": {
+              "enable": false,
+              "mode": "push"
+          },
+          "resize": true
+      },
+      "modes": {
+          "grab": {
+              "distance": 400,
+              "line_linked": {
+                  "opacity": 0.8014182674794185
+              }
+          },
+          "bubble": {
+              "distance": 158.35505639876231,
+              "size": 4.060386061506726,
+              "duration": 9.90734199007641,
+              "opacity": 0.7227487189481971,
+              "speed": 3
+          },
+          "repulse": {
+              "distance": 200,
+              "duration": 0.4
+          },
+          "push": {
+              "particles_nb": 4
+          },
+          "remove": {
+              "particles_nb": 2
+          }
+      }
+  },
+  "retina_detect": true
+});
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);; // This is just a sample script. Paste your real code (javascript or HTML) here.
 
-/*particlesJS("particles-js", {"particles":{"number":{"value":76,"density":{"enable":true,
+if ('this_is' == /an_example/) {
+  of_beautifier();
+} else {
+  var a = b ? (c % d) : e[f];
+}
+/*particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,
 "value_area":1420.4657549380909}
 },
 "color":{"value":"#fff"},
@@ -24,138 +156,3 @@
       { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } 
       requestAnimationFrame(update); }; requestAnimationFrame(update);;*/
 
-
-
-
-
-
-
-
-
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-
-
-/* Otherwise just put the config content (json): */
-
-('particles-js',
-  
-  {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ff0000"
-      },
-      "shape": {
-        "type": "triangle",
-        "stroke": {
-          "width": 0,
-          "color": "#ff0000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#808080",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": false,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
-
-);
