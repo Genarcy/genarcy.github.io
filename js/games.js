a71ea6a53891c4924036dc2312df766ce267647b
0rca <113267753+Genarcy@users.noreply.github.com>
2023-01-07 17:09:33 -0500

    d

--- js/games.js
+++ js/games.js
@@ -1,18 +1,5 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        function randomlink(){
+function randomlink(){
         randomlinks = document.getElementsByClassName("game")
         window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
         }
