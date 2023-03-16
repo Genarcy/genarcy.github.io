8ec342a3cecc58b2e5c91f7557c8a1ad72c055dc
0rca <113267753+Genarcy@users.noreply.github.com>
2023-03-16 20:50:56 +0000

    V4 duck queen elizabeth

--- js/sound-onclick.js
+++
@@ -1,6 +0,0 @@
-function playClip() {
-	if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8") != -1) && document.all) {
-		document.all.sound.src = "/audio/Genarcy.mp3";
-	} else document.getElementsByTagName("audio")[0].play();
-}
-
