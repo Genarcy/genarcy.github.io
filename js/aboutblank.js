7e1311f1e6d9441552fc5d296721414b816c35cd
0rca <113267753+Genarcy@users.noreply.github.com>
2023-04-03 14:00:16 -0400

    d

---
+++ js/aboutblank.js
@@ -0,0 +1,20 @@
+function openIframe() {
+    var url = window.location.href;
+    
+    var newTab = window.open('about:blank');
+
+    newTab.addEventListener('load', function() {
+        var iframe = document.createElement('iframe');
+
+        iframe.src = url;
+
+        iframe.style.position = "fixed";
+        iframe.style.top = "0";
+        iframe.style.left = "0";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+
+        // Add the iframe to the new tab
+        newTab.document.body.appendChild(iframe);
+    });
+}
