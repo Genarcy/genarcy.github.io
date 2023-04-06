3f890d68d2d2bced14eda4e54349ecc8b58602d6
0rca <113267753+Genarcy@users.noreply.github.com>
2023-04-06 00:32:47 -0400

    dsds

--- js/aboutblank.js
+++ js/aboutblank.js
@@ -1,10 +1,14 @@
 function loadPage() {
-  var iframe = document.createElement('iframe');
-  iframe.setAttribute('src', window.location.href);
-  iframe.style.position = 'absolute';
-  iframe.style.top = '0';
-  iframe.style.left = '0';
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
-  document.body.appendChild(iframe);
-}
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<html><head><title>Full-Screen Iframe</title></head><body style="margin:0;padding:0;"></body></html>');
+  
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', window.location.href);
+    iframe.style.position = 'absolute';
+    iframe.style.top = '0';
+    iframe.style.left = '0';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+  
+    newWindow.document.body.appendChild(iframe);
+  }
