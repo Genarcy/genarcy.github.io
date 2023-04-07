9245dfd1c0ec7a66753b18323047d67bb337f155
0rca <113267753+Genarcy@users.noreply.github.com>
2023-04-07 17:49:36 -0400

    f

--- js/main.js
+++ js/main.js
@@ -1,39 +1,9 @@
-function listener(event) {
-    switch(event.type) {
-      case "animationend":
-        console.log(`Ended: elapsed time is ${event.elapsedTime}`)
-        let foo = document.getElementById("box");
-        foo.remove();
-        document.getElementById('blank').style.animation="slide-out-blank 0.5s";
-        
-        
-        
-        
-        break;
-  
-    }
-  }
-  
-  const element = document.getElementById("box");
-  element.addEventListener("animationend", listener, false);
-  element.className = "box";
-  
-  
-  
-  function listener2(event) {
-    switch(event.type) {
-      case "animationend":
-        console.log(`Ended: elapsed time is ${event.elapsedTime}`)
-        let foe = document.getElementById("blank");
-        foe.remove();
-        
-        
-        
-        break;
-  
-    }
-  }
-  
-  const element2 = document.getElementById("blank");
-  element2.addEventListener("animationend", listener2, false);
-  element2.className = "blank";
+const gascript = document.createElement("script");
+gascript.setAttribute("async", "");
+gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-XWJJ5LHNR2");
+const inlinegascript = document.createElement("script");
+inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-XWJJ5LHNR2');`;
+document.head.append(gascript, inlinegascript);
