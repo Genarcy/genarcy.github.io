d921f20be9f4a4026cba45c996f34214df9ca508
0rca <113267753+Genarcy@users.noreply.github.com>
2023-06-24 19:28:47 -0400

    ultraciokt

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const gascript = document.createElement("script");
-gascript.setAttribute("async", "");
-gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-XWJJ5LHNR2");
-const inlinegascript = document.createElement("script");
-inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-XWJJ5LHNR2');`;
-document.head.append(gascript, inlinegascript);
